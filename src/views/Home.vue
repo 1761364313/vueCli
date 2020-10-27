2c15dbe27dc39b84b6084bd069ecedbc6e501474
wangyl1 <wangyl1@yunjiglobal.com>
2020-10-27 17:29:36 +0800

    config配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import ajax from 'axios'
 
 export default {
   name: 'Home',
@@ -19,7 +20,18 @@ export default {
       a: 'sad'
     }
   },
+  activated() {
+    this.getApi()
+  },
   methods: {
+    getApi() {
+      ajax.create({        
+        withCredentials: true
+      })  
+      ajax.get('//test.com/api/yunjiuserapp/userapp/member/memberInfo.json?appCont=1&ticket=ticket|2_25902c23bfe183bd317dfa8c4d3c58a2').then((res) => {
+        console.log(res)
+      })
+    },
     getFun() {
       const s = 112
       console.log(s)
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,47 @@
+const path = require('path')
+const argv = process.argv
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== 'production' ? 'error' : true
+  lintOnSave: process.env.NODE_ENV !== 'production' ? 'error' : true,
+  productionSourceMap: false, // 生产环境不需要sourceMap
+  crossorigin: 'anonymous',
+  outputDir: '/test',
+  publicPath: process.env.NODE_ENV == 'production' ? `//static.yunjiglobal.com/membercenter` : '/test',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'src': path.resolve(__dirname, './src'),
+        'common': path.resolve(__dirname, './src/common'),
+        'components': path.resolve(__dirname, './src/components'),
+        'store': path.resolve(__dirname, './src/store'),       
+        'assets': path.resolve(__dirname, './src/assets'),
+        'views': path.resolve(__dirname,'./src/views/')
+      }
+    }
+  },
+  devServer: {  
+    port: '8019',   
+    proxy: {
+      '/api': {
+        target: 'http://user-do-dev.yunjiglobal.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    disableHostCheck: true,
+    historyApiFallback: {
+      rewrites: [
+        { from: /\/test/, to: path.posix.join('/test', 'index.html') }
+      ],
+    }
+  }
 }
+
